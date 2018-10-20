0463f6f1b34b4dddcb4b54500a58b00d07580dfd
chrX <1026336374@qq.com>
2018-10-20 19:09:12 +0800

    1111

--- crun.py
+++ crun.py
@@ -15,21 +15,44 @@ if __name__=='__main__':
         'cmd':''
     }
 
+    #复制文件和安装docker，k8s等软件
+    '''
+    hosts=hostsM.hosts2
+    ssh_th.scpDir(hosts, dict1, '/home/nscc/', 'th')
+    cmd1='service docker stop ; /home/nscc/th/sh/setupSoft.sh && /home/nscc/th/sh/setSwap.sh && /home/nscc/th/sh/loadDockerImage.sh'
+    ssh_th.execCmd(hosts, dict1, cmd1)
+    '''
+
+    #配置docker使用试验特征已经使用etcd存储
+    '''
+    hosts=hostsM.hosts_cal2
+    ssh_th.execCmd(hosts, dict1, 'cp /home/nscc/th/calico-2.6.11/daemon.json /etc/docker/; systemctl daemon-reload; systemctl restart docker')
+    '''
+
     #k8s 开启 cadvisor (自能执行一次)
+    '''
     fileToEdit='/etc/systemd/system/kubelet.service.d/10-kubeadm.conf'
     cmd1="sed -i 's/$KUBELET_EXTRA_ARGS/$KUBELET_EXTRA_ARGS --cadvisor-port=4194/g' " + fileToEdit 
     ssh_th.execCmd(hostsM.hosts_k8s2, dict1, cmd1 + ';systemctl daemon-reload; systemctl restart kubelet')
-    
+    '''
+
     # 加入集群
+    '''
     hosts = [host for host in hostsM.hosts_k8s2 if host != '10.129.48.3']
     cmd1='kubeadm join 10.139.48.3:6443 --token 0j6p7w.tgie7rpflc9gxcor --discovery-token-ca-cert-hash sha256:2e0c39cf6e9684a673fe2d41a8bd56a98bfa3ef04c45ca4e2d827ab6eb2c1a7c'
     cmd2="sed -i 's/10.96.0.10/10.190.96.10/g' /var/lib/kubelet/config.yaml"
     cmd3="systemctl daemon-reload; systemctl restart kubelet"
     ssh_th.execCmd(hosts, dict1, '%s;%s;%s' % (cmd1, cmd2, cmd3))
-    
-    #ssh_th.scpDir(hostsM.hosts, dict1, '/home/nscc/th/', 'k8s-moni')
+    '''
+    # 复制,某个文件夹
+    #ssh_th.scpDir(hostsM.hosts, dict1, '/home/nscc/th/', 'calico-2.6.11')
+    #ssh_th.scpDir(hostsM.hosts, dict1, '/home/nscc/th/', 'sh')
 
+    #ssh_th.execCmd(hostsM.hosts, dict1, 'cd /home/nscc/th/tar.o; ls | xargs -n 1 docker image load -i')
     #ssh_th.scpDir(hostsM.hosts, dict1, '/home/nscc/th/', 'calico-3.2.3')
+
+    
+
 
     #execCmd(dict1, 'rm -rf /home/nscc/th')
     #scpDir(dict1, 'home/nscc/', 'th')
@@ -40,3 +63,4 @@ if __name__=='__main__':
     #execCmd(dict1, '/home/nscc/th/sh/loadDockerImage.sh')
     #execCmd(dict1, 'service docker stop && /home/nscc/th/sh/setupSoft.sh')
     #execCmd(dict1, 'service docker stop ; /home/nscc/th/sh/setupSoft.sh && /home/nscc/th/sh/setSwap.sh && /home/nscc/th/sh/loadDockerImage.sh')
+
